a2693fc89df0d37776b8b4014c200011ec9a7d2a
ChervonenkoDaniil <117937133+ChervonenkoDaniil@users.noreply.github.com>
2022-12-02 23:11:47 +0300

    Update task_3.py

--- Лабораторная работа_4/task_3.py
+++ Лабораторная работа_4/task_3.py
@@ -1,13 +1,12 @@
-def delete(list_, index=-1):
-    if index == -1:
-        list_ = list_[:index]
-    else:
-        list_ = list_[:index] + list_[index+1:]
+def delete(list_, index=None):
+    # используем cпособ list comprehension
+    list_ = (list_[:-1] if index is None else list_[:index] + list_[index + 1:])
     return list_
+
 
 # TODO реализовать функцию удаления элемента из списка по индексу
 
 
-print(delete([0, 1, 2], index=0))  # [0, 1]
+print(delete([0, 1, 2], index=0))  # [0, 1], ошибка, правильно [1,2]
 print(delete([0, 1, 2], index=1))  # [0, 2]
 print(delete([0, 1, 2]))  # [0, 1]
