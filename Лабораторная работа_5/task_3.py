90a056b454abacb1896626bd75124060e1b9eb5e
ChervonenkoDaniil <117937133+ChervonenkoDaniil@users.noreply.github.com>
2022-12-10 21:53:34 +0300

    Update task_3.py

--- Лабораторная работа_5/task_3.py
+++ Лабораторная работа_5/task_3.py
@@ -1,8 +1,15 @@
-from random import sample
+from random import choice
 
 
-def get_unique_list_numbers() -> list[int]:
-    return sample([i for i in range(-10, 10, 1)], 15)
+def get_unique_list_numbers(start=-10, stop=10, n=15) -> list[int]:  # Добавлено для универсальности решения
+    list_of_numbers = []  # Создаем список для заполнения
+    # Цикл while, т.к устанавливаем четкие границы работы цикла c выполнением условия отсутствия повтора чисел в списке
+    # При использовании цикла for, цикл продолжался, теряя позиции в списке при повторе переменной number
+    while len(list_of_numbers) != n:
+        number = choice([i for i in range(start, stop+1)])  # Вызываем случайное число из интервала
+        if number not in list_of_numbers:
+            list_of_numbers.append(number)
+    return list_of_numbers
 # TODO написать функцию для получения списка уникальных целых чисел
 
 
